40e2a3f6213e8f2d4c74401101339ea4883e4b0e
RiEm9 <59747420+RiEm9@users.noreply.github.com>
2026-06-29 03:28:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-setTimeout(()=>{
 
-document.getElementById("intro").style.opacity="0";
-
-document.getElementById("main").style.opacity="1";
-
-},2300);
-
-setTimeout(()=>{
-
-document.getElementById("intro").style.display="none";
-
-},3500);
