0207274c3d6270350e533a5976aba258e0fff47f
RiEm9 <59747420+RiEm9@users.noreply.github.com>
2026-06-29 03:21:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+setTimeout(()=>{
 
+document.getElementById("intro").style.opacity="0";
+
+document.getElementById("main").style.opacity="1";
+
+},2300);
+
+setTimeout(()=>{
+
+document.getElementById("intro").style.display="none";
+
+},3500);
